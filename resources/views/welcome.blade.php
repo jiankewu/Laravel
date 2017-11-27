112926b27d180ee2754197a75aea38df1fbe4ee7
wujianke <691723849@qq.com>
2017-11-27 11:38:39 +0800

    默认页面恢复

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Hello Laravel
+                    Laravel
                 </div>
 
                 <div class="links">
